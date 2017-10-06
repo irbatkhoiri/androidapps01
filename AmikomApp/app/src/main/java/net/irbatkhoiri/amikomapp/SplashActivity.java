a72655636fc5b874ee19a24845ec19a7c0460b75
irbatkhoiri <irbat.khoiri@students.amikom.ac.id>
2017-10-06 10:13:39 +0700

    spalshactivity

--- AmikomApp/app/src/main/java/net/irbatkhoiri/amikomapp/SplashActivity.java
+++ AmikomApp/app/src/main/java/net/irbatkhoiri/amikomapp/SplashActivity.java
@@ -1,5 +1,6 @@
 package net.irbatkhoiri.amikomapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,27 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent intent= new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timerThread.start();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
     }
 }
+
